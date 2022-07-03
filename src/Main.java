a9d32f7bee09282cca6a6b04ecd4815998ed58cc
boburmirzaqulov <ab200mirzaqulov@gmail.com>
2022-07-03 11:51:14 +0500

    Task2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,27 @@
 import tasks.Task1;
+import tasks.Task2;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //TASK 1
         int[][] nums = new int[][]{
+                {0,1,0,0,0,0,0},
                 {0,0,0,0,0,0,0},
                 {0,0,0,0,0,0,0},
                 {0,0,0,0,0,0,0},
                 {0,0,0,0,0,0,0},
-                {0,0,0,1,0,0,0},
                 {0,0,0,0,0,0,0},
                 {0,0,0,0,0,0,0}
         };
         System.out.println(Task1.getStepCount(nums));
+
+        //TASK 2
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number for Task-2:");
+        System.out.println(Task2.lastDigit(scanner.nextInt()));
+
+
     }
 }
---
+++ src/tasks/Task2.java
@@ -0,0 +1,8 @@
+package tasks;
+
+public class Task2 {
+    public static int lastDigit(int n){
+        if (n>=5) return 1;
+        return 0;
+    }
+}
